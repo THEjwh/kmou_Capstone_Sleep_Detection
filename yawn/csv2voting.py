1ea24a0d27b4645961aa552bb74d07332ff9937c
lee ji hyeon <marosemerry1097@gmail.com>
2022-11-29 16:58:32 +0900

    c최종이어요

--- yawn/csv2voting.py
+++ yawn/csv2voting.py
@@ -14,37 +14,59 @@ import numpy as np
 
 # mouth_data= pd.concat([no_yawn,yawn],axis=0)
 
+# #pandas로 읽기
 mouth_data = pd.read_csv("mouth_data.csv")
 
+#데이터 섞기
 from sklearn.utils import shuffle
 mouth_data = shuffle(mouth_data,random_state=26)
 
+#데이터을 배열로 만들기
 train=mouth_data['rate'].to_numpy()
 test=mouth_data['state'].to_numpy()
-
 
 #train, test 만들기
 from sklearn.model_selection import train_test_split
 train_input,test_input,train_target,test_target = train_test_split(train,test,test_size=0.2)
 
+###데이터 모습 보기
+##import matplotlib.pyplot as plt
+##plt.scatter(train_input,train_target)
+##plt.xlabel('rate')
+##plt.ylabel('state')
+##plt.show()
+###그림이 매우 극단적임. 게다가 겹치는 곳도 너무 많고. 0.5~1.0 겹치다니... 이런 경우는 선형회귀가 될까...
 
+#차원조정하기
 train_input = train_input.reshape(-1,1)
 test_input = test_input.reshape(-1,1)
 
 
+#데이터 정규화 단계  -- 릿지랑 라쏘 하려면 꼭 데이터를 정규화 해줘야함
+from sklearn.preprocessing import StandardScaler
+ss=StandardScaler()
+ss.fit(train_input)
+train_input = ss.transfrom(train_input)
+test_input = ss.transform(test_input)
+
+
+# print("\n랜덤포레스트\n")
+# from sklearn.ensemble import RandomForestRegressor
+
+
+print("\n회귀트리 코드\n")
 #회귀트리 코드 & 그리드 서치
-#from scipy.stats import uniform,randint
+#from scipy.stats import uniform,randint  #하고싶었는데 안됨. 이유가 뭐임??
 from sklearn.model_selection import GridSearchCV
 #from xgboost import XGBClassifier
-#from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.ensemble import ExtraTreesRegressor
-
-ex=ExtraTreesRegressor()
-params={'n_estimators' :np.arange(500), 
-        #'learning_rate': np.arange(0.1,1,0.1),
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import accuracy_score
+gb =GradientBoostingRegressor()
+params={'n_estimators' :np.arange(300), 
+        'learning_rate': np.arange(0.1,1,0.1),
         'max_depth':np.arange(10) 
         }
-grid = GridSearchCV(ex,params,n_jobs =-1)
+grid = GridSearchCV(gb,params,n_jobs =-1)
 grid.fit(train_input,train_target)
 print(grid.best_params_)  #최상 조합 출력
 print(np.max(grid.cv_results_['mean_test_score']))
@@ -53,13 +75,48 @@ print(np.max(grid.cv_results_['mean_test_score']))
 dt=grid.best_estimator_
 print(dt.score(test_input,test_target))
 
-            
+##print("\n선형회귀\n")
+###선형회귀 
+##from sklearn.linear_model import LinearRegression
+##lr=LinearRegression()
+##lr.fit(train_input,train_target)
+##print(lr.score(test_input,test_target))
+##predictions = lr.predict(test_input)
+##accuracy_score(test_target,predictions)
+##
+##
+##print("\n릿지\n")
+###릿지랑 라쏘를 해보자요
+##from sklearn.linear_model import Ridge,Lasso
+##ridge = Ridge(random_state=26)
+##params = {'alpha_list':np.arange(0.01,10,0.01)}
+##grid = GridSearchCV(ridge,params,n_jobs =-1)
+##grid.fit(train_input,train_target)
+##print(grid.best_params_)  #최상 조합 출력
+##print(np.max(grid.cv_results_['mean_test_score']))
+##
+###최적의 모델은 best_estimator_ 속에 저장되어 있음
+##dt=grid.best_estimator_
+##print(dt.score(test_input,test_target))
+##
+##print("\n라쏘\n")
+###라쏘
+##lasso = Lasso(random_state=26)
+##grid_L = GridSearchCV(lasso,params,n_jobs =-1)
+##grid_L.fit(train_input,train_target)
+##print(grid_L.best_params_)  #최상 조합 출력
+##print(np.max(grid_L.cv_results_['mean_test_score']))
+##
+###최적의 모델은 best_estimator_ 속에 저장되어 있음
+##dt2=grid_L.best_estimator_
+##print(dt2.score(test_input,test_target))
 
 
-# #보팅
-# from sklearn.ensemble import VotingClassifier
-# from sklearn.metrics import accuracy_score
-# voting_result = VotingClassifier(estimators=[("추정기 이름",추정기),(),()],weights=[비율,,],voting='soft'.fit(train_input,target_input))
-# soft_voting_predicted = voting_result.predict(test_input)
-# #accuracy_score(test_target, soft_voting_predicted) 정확도 보는 것
+
+# # #보팅
+#  from sklearn.ensemble import VotingClassifier
+#  from sklearn.metrics import accuracy_score
+#  voting_result = VotingClassifier(estimators=[("눈",추정기),("고개",),('입',)],weights=[비율,,],voting='soft'.fit(train_input,target_input))
+#  soft_voting_predicted = voting_result.predict(test_input)
+#  #accuracy_score(test_target, soft_voting_predicted) 정확도 보는 것
 
